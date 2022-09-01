8ae105c1da99104c01c791b9876fbc78e4970eda
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-09-02 01:15:26 +0700

    connect upload files to be

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["api.elements.buildwithangga.com"]
+    domains: ["api.elements.buildwithangga.com", "res.cloudinary.com"]
   }
 }
 module.exports = nextConfig
